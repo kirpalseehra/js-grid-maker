98697612f35069924c3f2ea768271a158b737935
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2018-11-29 09:04:08 +0000

    tried to do this way with appending child but it didn't work

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ document.addEventListener('DOMContentLoaded', function () {
   // Add code here
   const board = document.getElementById('board');
 
-  function createGrid(x) {
-    for (var rows = 0; rows < 3; rows++) {
-      for (var cols = 0; cols < 3; cols++) {
-        $("#board").append("<div id='board'></div>");
-      };
-    };
-    $(".cols").width(960 / x);
-    $(".rows").height(960 / x);
-  };
+  // function createGrid(x) {
+  //   for (var rows = 0; rows < 3; rows++) {
+  //     for (var cols = 0; cols < 3; cols++) {
+  //       $("#board").append("<div id='board'></div>");
+  //     };
+  //   };
+  //   $(".cols").width(960 / x);
+  //   $(".rows").height(960 / x);
+  // };
+
+  function genDivs(v) {
+    var e = document.body; // whatever you want to append the rows to: 
+    for (var i = 0; i < v; i++) {
+      var row = document.createElement("div");
+      row.className = "row";
+      for (var x = 1; x <= v; x++) {
+        var cell = document.createElement("div");
+        cell.className = "gridsquare";
+        cell.innerText = (i * v) + x;
+        row.appendChild(cell);
+      }
+      e.appendChild(row);
+    }
+    document.getElementById("code").innerText = e.innerHTML;
 
 
 
 
-});
+  });
+
+genDivs();
