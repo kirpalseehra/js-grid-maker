1f97fb4db260565bbdf7817283741d821a7e57da
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2018-11-29 09:11:00 +0000

    got the blocks to display but they're in a column and not centered

--- app.js
+++ app.js
@@ -12,24 +12,36 @@ document.addEventListener('DOMContentLoaded', function () {
   //   $(".rows").height(960 / x);
   // };
 
-  function genDivs(v) {
-    var e = document.body; // whatever you want to append the rows to: 
-    for (var i = 0; i < v; i++) {
-      var row = document.createElement("div");
-      row.className = "row";
-      for (var x = 1; x <= v; x++) {
-        var cell = document.createElement("div");
-        cell.className = "gridsquare";
-        cell.innerText = (i * v) + x;
-        row.appendChild(cell);
-      }
-      e.appendChild(row);
+  // function genDivs(v) {
+  //   var e = document.body; // whatever you want to append the rows to: 
+  //   for (var i = 0; i < v; i++) {
+  //     var row = document.createElement("div");
+  //     row.className = "row";
+  //     for (var x = 1; x <= v; x++) {
+  //       var cell = document.createElement("div");
+  //       cell.className = "gridsquare";
+  //       cell.innerText = (i * v) + x;
+  //       row.appendChild(cell);
+  //     }
+  //     e.appendChild(row);
+  //   }
+  //   document.getElementById("code").innerText = e.innerHTML;
+
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+      var div = document.createElement("div");
+      div.style.width = "30px";
+      div.style.height = "30px";
+      div.style.background = "black";
+
+      var jump = document.createElement("br");
+      document.getElementById("board").appendChild(jump);
+      document.getElementById("board").appendChild(div);
     }
-    document.getElementById("code").innerText = e.innerHTML;
+  }
 
 
 
-
-  });
+});
 
 genDivs();
